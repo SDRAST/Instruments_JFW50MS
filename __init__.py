53b4a09490d09855d395574a428fd6b78adbaded
Tom Kuiper <kuiper@jpl.nasa.gov>
2017-03-25 16:21:47 -0700

    converted to server/client scheme

--- __init__.py
+++ __init__.py
@@ -6,41 +6,24 @@ import logging
 import re
 
 from MonitorControl import ObservatoryError, Device, Switch, show_port_sources
+from Electronics.Instruments.JFW50MS.hwif import MS287
+from support.pyro import get_device_server
 
 module_logger = logging.getLogger(__name__)
 
-class MS287(Device):
+class MS287client(Device):
   """
-  This is a 24 input by 4 output full matrix switch.
+  This is the client class for a 24 input by 4 output full matrix switch.
 
-  The switch numbers its ports 1...N.  For Python we convert it to (0...N-1).
-  We can treat it logically as four 24x1 switches.  Some functions apply to the
-  entire physical switch. To avoid repeating them we have a class variable
-  'connection'.  For now, this limits us to one switch of this type in a
-  configuration but we can change it to a dict to support multiple JFW50MS287
-  switches.
-
-  Class Variable
-  ==============
-  connection  - a Telnet instance for the session with the physical switch.
-  host        - assigned by sys admin
-  port        - manufacture's default
-  switch_type - apply as Switch().sw_type.
+  The actual switch is controlled by a server through the class MS287server 
+  which is defined in the server app MS287_server.py.  The switch itself is a
+  sub-class of class MS287 in the module 'hwif'.
   
-  Public attributes
-  =================
-  These additional attributes are added to the Switch() atrributes of sw_type,
-  sources and destinations::
-    - name   - identifier text for switch
-    - switch - switch number = outport number - 1
-    - state  - index into multiports[] to active input port
+  The purpose of this module is to manage the signal flow.
   """
   switch_type = "Nx1"
-  host = "192.168.100.50"
-  port = 3001
-  connection = None
 
-  def __init__(self, parent, name, inputs=None, output_names=[], active=True):
+  def __init__(self, name, inputs=None, output_names=[], active=True):
     """
     In most Device types, the first argument is the device providing the
     signal source.  In the case of a Switch it is the instance which "owns" the
@@ -62,47 +45,34 @@ class MS287(Device):
     self.logger = mylogger
     show_port_sources(inputs, " MS287 inputs redefined:", mylogger.level)
     self.name = name
-    self.parent = parent
-    self._connect()
+    self.hw = get_device_server("MS287server")
+    
     self.channel = {}
     for name in output_names:
       self.channel[name] = self.Channel(self, name, inputs=self.inputs,
                                         output_names = [name], active=active)
     self.logger.debug(" %s outputs: %s", self.name, self.outputs)
 
-  def _connect(self):
+  def get_states(self):
     """
     """
-    try:
-      MS287.connection = telnetlib.Telnet(host=MS287.host,
-                                               port=MS287.port)
-    except Exception, details:
-      self.logger.error("_connect: Could not connect to MS287: %s",
-                     str(details))
-      raise ObservatoryError(MS287.host,"did not respond. IF switch is down.")
-    else:
-      received = MS287.connection.read_until('something',0.5)
-      self.logger.debug("_connect: MS287 replied: %s",received.strip())
-
-  def send_request(self,request):
+    return self.hw.get_states()
+  
+  def help(self):
     """
-    Send a command and get the response.
-
-    The read_all() method doesn't work because the switch does not send an
-    EOF at the end of a response.
-
-    @param request : command to be sent to the switch
-    @type  request : str
-
-    @return: response from switch (str)
     """
-    MS287.connection.write(request+'\r')
-    response = MS287.connection.read_until('something',0.5)
-    self.logger.debug("send_request: returned: %s",response.strip() )
-    if re.search("Error", response):
-      raise ObservatoryError(response,"returned by MS287")
-    return response
-
+    return self.hw.help()
+    
+  def set_state(self, switch, source):
+    """
+    """
+    self.hw.set_state(switch, source)
+  
+  def get_state(self, switch):
+    """
+    """
+    self.hw.get_state(switch)
+    
   class Channel(Switch):
     def __init__(self, parent, name, inputs=None, output_names=None,
                  active=True):
@@ -112,7 +82,7 @@ class MS287(Device):
       self.ID = int(name[-1])-1
       mylogger = logging.getLogger(self.parent.logger.name+".Channel")
       show_port_sources(inputs, " Inputs to %s:" % self, mylogger.level)
-      Switch.__init__(self, parent, name, inputs=inputs,
+      Switch.__init__(self, name, inputs=inputs,
                       output_names=[name], stype = self.type,
                       active=active)
       self.logger = mylogger
@@ -120,37 +90,3 @@ class MS287(Device):
                          self.logger.level)
       self.parent.outputs[name] = self.outputs[name]
                       
-    def _get_state(self):
-      """
-      Returns the state of the switch channel
-
-      The switch state is the 0-based number of the input which is connected to
-      the output, e.g., 0 means input #1.
-
-      A typical response from the MS287 is "Output #1 set to Input #4".  An
-      input of #0 means the output is set to its self-terminating state. This
-      results in a state of -1.
-      """
-      command = "RO"+str(self.ID+1)
-      self.logger.debug("_get_state: command sent: %s", command)
-      response = self.parent.send_request(command).strip()
-      self.logger.debug("_get_state: response received: %s", response)
-      # Parse the last word for the number
-      inport_str = response.split()[-1]
-      inport = int(inport_str.lstrip("#"))
-      self.logger.debug("_get_state: inport = %s", inport)
-      self.state = int(inport)-1
-      return self.state
-
-    def _set_state(self,inport):
-      """
-      Sets the state of the switch channel
-      """
-      command = "SOR%1d %2d" % (self.ID+1,inport)
-      self.logger.debug("set_input: Command sent: %s", command)
-      response = self.parent.send_request(command)
-      inport_str = response.strip().split()[-1]
-      inport = int(inport_str.lstrip("#"))
-      self.logger.debug("set_input: Response: %s", inport)
-      self.state = int(inport)-1
-      return self.state
--- apps/server/MS287_server.py
+++ apps/server/MS287_server.py
@@ -6,33 +6,70 @@ from support.logs import init_logging, get_loglevel, set_loglevel
 from support.pyro import PyroServer, PyroServerLauncher
 
 from Electronics.Instruments.JFW50MS.hwif import MS287
+       
+examples = '''
+help         - returns this text
+set_state    - selects the input (1-24) for a switch; returns get_state()
+get_state    - returns the state of the switch = input port number-1
+send_request - process a MS287 command; returns MS287 response
+quit         - stops the Pyro server
+'''
 
 # Set up Python logging
 logging.basicConfig(level=logging.WARNING)
 module_logger = logging.getLogger(__name__)
 
 class MS287_Server(PyroServer, MS287):
+  """
+  """
+  def __init__(self):
     """
     """
-    def __init__(self):
+    self.logger = logging.getLogger(module_logger.name+".MS287_Server")
+    PyroServer.__init__(self)
+    self.logger.debug(" superclass initialized")
+    MS287.__init__(self)
+    self.logger.debug(" server instantiated")
+    self.run = True
+
+  def set_state(self, number, inp):
+    """
+    Selects the input for a switch.
+    
+    Returns the state of the switch which is the input port number - 1
+    
+    @param number : number of the switch, 1-4
+    @type  number : int or str
+    
+    @return: int
+    """
+    state = int(inp)
+    self.channel[str(number)]._set_state(state)
+    return self.channel[str(number)]()
+    
+  def get_state(self, number):
+    """
+    """
+    self.channel[str(number)]._get_state()
+    return self.channel[str(number)].state
+    
+  def quit(self):
       """
       """
-      self.logger = logging.getLogger(module_logger.name+".MS287_Server")
-      #super(MS287_Server,self).__init__()
-      PyroServer.__init__(self)
-      self.logger.debug(" superclass initialized")
-      MS287.__init__(self)
-      self.logger.debug(" server instantiated")
-      self.run = True
- 
-    def answer(self):
-      return "got it"
-       
-examples = ''
-  
+      self.logger.info("quit: stopping daemon")
+      self.run = False
+      self.halt()
+
+  def help(self):
+    """
+    """
+    return examples
+    
 def main():
     """
     """
+    name = "MS287server"
+    
     p = initiate_option_parser(
      """Pyro server for MS287 IF switch.""",
      examples)
@@ -44,11 +81,11 @@ def main():
     mylogger = init_logging(logging.getLogger(),
                             loglevel   = get_loglevel(opts.file_loglevel),
                             consolevel = get_loglevel(opts.stderr_loglevel),
-                            logname    = opts.logpath+__name__+".log")
+                            logname    = opts.logpath+name+".log")
     mylogger.debug(" Handlers: %s", mylogger.handlers)
     loggers = set_module_loggers(eval(opts.modloglevels))
 
-    psl = PyroServerLauncher("MS287server", nameserver_host='dto')
+    psl = PyroServerLauncher(name, nameserver_host='dto')
     IFsw = MS287_Server()
     psl.start(IFsw)
   
